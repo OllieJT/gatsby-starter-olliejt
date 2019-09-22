fdc9fec87c2486eaa6dd5309f09c5381bbf1a4bd
Ollie Taylor <contact@olliejt.com>
2019-09-22 15:01:41 +0200

    Improved Boilerplate

---
+++ src/components/Page/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Container from "../Container"
+
+const Footer = () => (
+	<Container restrict as="footer" role="contentinfo">
+		<p>
+			Made by <a href="http://twitter.com/TheOllieJT">@TheOllieJT</a>.
+		</p>
+	</Container>
+)
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,42 +2,42 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import { setThemeColor } from "../utility/functions"
+import Header from "../components/Page/Header"
+import Demo from "../components/Demo"
 
 const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO
-      title="OllieJT"
-      keywords={[
-        `portfolio`,
-        `ollie`,
-        `taylor`,
-        `olliejt`,
-        `theolliejt`,
-        `designer`,
-        `product designer`,
-        `esports`,
-        `gaming`,
-      ]}
-    />
-    <h1>Homepage</h1>
-  </Layout>
+	<Layout theme={setThemeColor("switch-invert")}>
+		<SEO
+			title="OllieJT"
+			keywords={[
+				`portfolio`,
+				`ollie`,
+				`taylor`,
+				`olliejt`,
+				`theolliejt`,
+				`designer`,
+				`product designer`,
+				`esports`,
+				`gaming`,
+			]}
+		/>
+		<Header />
+		<Demo />
+	</Layout>
 )
 
 export const query = graphql`
-  query homePageQuery {
-    allFile(
-      filter: {
-        name: { in: ["avatar-thailand", "photo-shanghai-s", "hero-video"] }
-      }
-    ) {
-      edges {
-        node {
-          name
-          relativePath
-        }
-      }
-    }
-  }
+	query homePageQuery {
+		allFile(filter: { name: { in: ["avatar-thailand", "photo-shanghai-s", "hero-video"] } }) {
+			edges {
+				node {
+					name
+					relativePath
+				}
+			}
+		}
+	}
 `
 
 export default IndexPage
