543aceb37d56cf37b5f43ac61e56c17d161ff6d4
Ollie Taylor <contact@olliejt.com>
2019-09-22 17:58:21 +0200

    Improved Utility

--- src/utility/functions.js
+++ src/utility/functions.js
@@ -139,3 +139,14 @@ setThemeColor.propTypes = {
 setThemeColor.defaultProps = {
 	s: PropTypes.oneOf(["switch", "switch-invert", "invert", "none"]),
 }
+
+export function slugify(text) {
+	return text
+		.toString()
+		.toLowerCase()
+		.replace(/\s+/g, "-") // Replace spaces with -
+		.replace(/[^\w\-]+/g, "") // Remove all non-word chars
+		.replace(/\-\-+/g, "-") // Replace multiple - with single -
+		.replace(/^-+/, "") // Trim - from start of text
+		.replace(/-+$/, "") // Trim - from end of text
+}
---
+++ src/utility/styles.js
@@ -0,0 +1,11 @@
+import { css } from "styled-components"
+
+export const cover = css`
+	position: absolute;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	width: 100%;
+	height: 100%;
+`
