35d84e222379ee698bae6391626515bde6b3851e
Ollie Taylor <contact@olliejt.com>
2019-09-22 15:01:19 +0200

    Improved reset and theming

--- src/utility/GlobalStyles.js
+++ src/utility/GlobalStyles.js
@@ -1,47 +1,207 @@
 import { createGlobalStyle } from "styled-components"
-import { color, size, layout } from "./theme"
+import { layout } from "./theme"
+import { setThemeColor } from "./functions"
+//import "./reset.css"
 
 const GlobalStyle = createGlobalStyle`
 	:root {
-		--colour-primary-050: #FFC2C2;
-		--colour-primary-100: #FF9999;
-		--colour-primary-200: #FF7A70;
-		--colour-primary-300: #FF5447;
-		--colour-primary-400: #FF3D1F;
-		--colour-primary-500: #FF1100;
-		--colour-primary-600: #B81800;
-		--colour-primary-700: #8F0013;
-		--colour-primary-800: #66000E;
-
-		--colour-secondary-050: #C2C2FF;
-		--colour-secondary-100: #9999FF;
-		--colour-secondary-200: #9585FF;
-		--colour-secondary-300: #6047FF;
-		--colour-secondary-400: #5A1FFF;
-		--colour-secondary-500: #3300FF;
-		--colour-secondary-600: #1E00E0;
-		--colour-secondary-700: #0C00B8;
-		--colour-secondary-800: #11118F;
-
-		--colour-level-000: hsl(0,0,100);
-		--colour-level-050: hsl(0,0,70);
-		--colour-level-100: hsl(0,0,60);
-		--colour-level-200: hsl(0,0,50);
-		--colour-level-300: hsl(0,0,40);
-		--colour-level-400: hsl(0,0,30);
-		--colour-level-500: hsl(0,0,20);
-		--colour-level-600: hsl(0,0,15);
-		--colour-level-700: hsl(0,0,10);
-		--colour-level-800: hsl(0,0,5);
-		--colour-level-900: hsl(0,0,0);
+		${setThemeColor("none")};
 	}
-	body,html{
-		-ms-overflow-style: none;
-		overflow: -moz-scrollbars-none;
-		&::-webkit-scrollbar{
-			display: none;
-		}
+
+	* {
+		box-sizing: border-box;
 	}
+
+	::before,
+	::after {
+		box-sizing: inherit;
+	}
+
+
+	html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		font-size: 100%;
+		font: inherit;
+		vertical-align: baseline;
+	}
+
+	article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section {
+		display: block;
+	}
+	ol,
+	ul {
+		list-style: none;
+	}
+	blockquote,
+	q {
+		quotes: none;
+	}
+	blockquote:before,
+	blockquote:after,
+	q:before,
+	q:after {
+		content: "";
+		content: none;
+	}
+	table {
+		border-collapse: collapse;
+		border-spacing: 0;
+	}
+
+	/**********
+	Forms
+	**********/
+
+	button,
+	input,
+	optgroup,
+	select,
+	textarea {
+		-webkit-appearance: none;
+		appearance: none;
+		vertical-align: middle;
+		color: inherit;
+		font: inherit;
+		border: inherit;
+		background: transparent;
+		padding: 0;
+		margin: 0;
+		outline: 0;
+		border-radius: 0;
+		text-align: inherit;
+	}
+
+	[type="checkbox"] {
+		-webkit-appearance: checkbox;
+		appearance: checkbox;
+	}
+
+	[type="radio"] {
+		-webkit-appearance: radio;
+		appearance: radio;
+	}
+
+	button,
+	input {
+		overflow: visible;
+	}
+
+	button,
+	select {
+		text-transform: none;
+	}
+
+	button,
+	[type="button"],
+	[type="reset"],
+	[type="submit"] {
+		cursor: pointer;
+		-webkit-appearance: none;
+		appearance: none;
+	}
+
+	button[disabled],
+	[type="button"][disabled],
+	[type="reset"][disabled],
+	[type="submit"][disabled] {
+		cursor: default;
+	}
+
+	button::-moz-focus-inner,
+	[type="button"]::-moz-focus-inner,
+	[type="reset"]::-moz-focus-inner,
+	[type="submit"]::-moz-focus-inner {
+		border-style: none;
+		padding: 0;
+	}
+
+	button:-moz-focusring,
+		[type="button"]:-moz-focusring,
+		[type="reset"]:-moz-focusring,
+		[type="submit"]:-moz-focusring {
+		outline: 1px dotted ButtonText;
+	}
+
+	fieldset {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		min-width: 0;
+	}
+
+	legend {
+		color: inherit;
+		display: table;
+		max-width: 100%;
+		padding: 0;
+		white-space: normal;
+	}
+
+	progress {
+		vertical-align: baseline;
+	}
+
+	textarea {
+		overflow: auto;
+	}
+
+	[type="checkbox"],
+	[type="radio"] {
+		padding: 0; /* 1 */
+	}
+
+
+	[type="number"]::-webkit-inner-spin-button,
+	[type="number"]::-webkit-outer-spin-button {
+		height: auto;
+	}
+
+	[type="search"] {
+		outline-offset: -2px; /* 1 */
+	}
+
+
+	[type="search"]::-webkit-search-decoration {
+		-webkit-appearance: none;
+	}
+
+	::-webkit-file-upload-button {
+		-webkit-appearance: button; /* 1 */
+		font: inherit; /* 2 */
+	}
+
+
+	label[for] {
+		cursor: pointer;
+	}
+
+
+	/**********
+	Embedded content
+	**********/
+
+	img {
+		border-style: none;
+		vertical-align: bottom;
+	}
+
+	embed,
+	object,
+	iframe {
+		border: 0;
+		vertical-align: bottom;
+	}
+
+	[hidden] {
+		display: none;
+	}
+
+
+	/**********
+	Globals
+	**********/
 
 	html{
 		width: 100%;
@@ -50,30 +210,28 @@ const GlobalStyle = createGlobalStyle`
 		font-size: 62.5%;
 		text-rendering: optimizeLegibility;
 		-webkit-font-smoothing: antialiased;
+		-webkit-text-size-adjust: 100%;
+		-webkit-tap-highlight-color: transparent;
 		@media (max-width: 600px) {
 			font-size: 48%;
 		}
 	}
+
+
 	body{
-		font-family: Chakra Petch, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif;
+		font-family: ${layout.text.font}, -apple-system, BlinkMacSystemFont, sans-serif;
 		font-style: normal;
 		font-weight: normal;
-		line-height: 0;
 		height: auto;
 		min-height: 100vh;
 		width: 100%;
 		max-width: 100vw;
-		background-color: ${layout.background};
-		color: ${layout.text.content};
+		background-color: ${layout.background.page};
+		color: ${layout.text.contentColor};
 		position: relative;
-		overflow: visible;
-		box-sizing: border-box;
-		margin: 0;
-		padding: 0;
-		*{
-			box-sizing: border-box;
-		}
+		line-height: 1;
 	}
+
 
 	/**********
 	Links
@@ -81,23 +239,31 @@ const GlobalStyle = createGlobalStyle`
 	a{
 		display: inline-block;
 		text-decoration: none;
-		color: ${layout.link.before};
+		cursor: pointer;
+		color: ${layout.link.default};
 		&:hover{
-			color: ${layout.link.after};
+			color: ${layout.link.hover};
+		}
+		&:focus{
+			color: ${layout.link.focus};
+			text-decoration: underline;
+		}
+		&:visited{
+			color: ${layout.link.visited};
 		}
 	}
 
 	/**********
-	Fallback
+	Typography
 	**********/
-	h1,h2,h3,h4,h5,h6,p,button{
+	h1,h2,h3,h4,h5,h6,p{
 		font-weight: 400;
 		-webkit-margin-before: 0;
 		-webkit-margin-after: 0;
-		line-height: 1.2em;
+		line-height: 1.15em;
 		&strong,&b{font-weight: 600}
 	}
-	h1,h2,h3,h4,h5,h6{color: ${layout.text.title}}
+	h1,h2,h3,h4,h5,h6{color: ${layout.text.titleColor}}
 	h1 {font-size: 6.4rem}
 	h2 {font-size: 5.6rem}
 	h3 {font-size: 4.8rem}
@@ -108,96 +274,23 @@ const GlobalStyle = createGlobalStyle`
 		letter-spacing: 0.04em;
 		text-transform: uppercase;
 	}
-	p,li,p{font-size:2rem}
+	p,li{font-size:2rem}
 
-	article{
-		h1,
-		h2,
-		h3,
-		h4,
-		h5,
-		h6,
-		p {
-			display: inline;
-			&:first-child {
-			margin-top: 0;
-			}
-			&:last-child {
-			margin-bottom: 0;
-			}
-		}
-		h1,
-		h2,
-		h3,
-		h4,
-		h5,
-		h6 {
-			margin: ${size.medium} 0;
-		}
-		p {
-			margin: ${size.xsmall} 0;
-		}
-		ul,
-		li {
-			display: block;
-			list-style-type: disc; /* decimal-leading-zero - decimal - disc - circle - square*/
-			list-style-position: inside;
-			list-style-image: none;
-		}
-		li {
-			flex-direction: row;
-			padding: 0.8rem 0;
-		}
-		a,
-		em {
-			color: ${color.primary["400"]};
-		}
-		a:hover {
-			color: ${props => props.secondaryColour};
-		}
-	}
-
-	blockquote{
-		-webkit-margin-before: 0;
-		-webkit-margin-after: 0;
-		-webkit-margin-start: 0;
-		-webkit-margin-end: 0;
-	}
-	ul,ol{
-		display: inline-block;
-		list-style: none;
+	ul,ol,blockquote{
 		-webkit-margin-before: 0;
 		-webkit-margin-after: 0;
 		-webkit-margin-start: 0;
 		-webkit-margin-end: 0px;
 		-webkit-padding-start: 0;
+		-webkit-padding-end: 0;
 	}
 
 
 
 
-	/*  UPDATE  */
 
-	header{
-		position: relative;
-		width: 100vw;
-		max-width: 100%;
-	}
-	.container{
-		position: relative;
-		z-index: 10;
-		display: block;
-		width: 100%;
-		max-width: ${layout.width.contentMinusGutter};
-		padding: ${layout.width.gutter};
-		margin-left: auto;
-		margin-right: auto;
-		box-sizing: border-box;
-      @media (max-width: 600px) {
-		  max-width: auto;
-		  padding: 1.6rem;
-      }
-	}
+
+
 `
 
 export default GlobalStyle
--- src/utility/functions.js
+++ src/utility/functions.js
@@ -1,1 +1,141 @@
+import PropTypes from "prop-types"
+import { color } from "./theme"
+
 export const IS_BROWSER = typeof window !== "undefined"
+
+export function setThemeColor(s) {
+	switch (s) {
+		case "switch":
+			return `
+	  --color-primary-800: ${color.secondary[0]};
+	  --color-primary-700: ${color.secondary[1]};
+	  --color-primary-600: ${color.secondary[2]};
+	  --color-primary-500: ${color.secondary[3]};
+	  --color-primary-400: ${color.secondary[4]};
+	  --color-primary-300: ${color.secondary[5]};
+	  --color-primary-200: ${color.secondary[6]};
+	  --color-primary-100: ${color.secondary[7]};
+	  --color-primary-050: ${color.secondary[8]};
+
+	  --color-secondary-050: ${color.primary[0]};
+	  --color-secondary-100: ${color.primary[1]};
+	  --color-secondary-200: ${color.primary[2]};
+	  --color-secondary-300: ${color.primary[3]};
+	  --color-secondary-400: ${color.primary[4]};
+	  --color-secondary-500: ${color.primary[5]};
+	  --color-secondary-600: ${color.primary[6]};
+	  --color-secondary-700: ${color.primary[7]};
+	  --color-secondary-800: ${color.primary[8]};
+	`
+		case "switch-invert":
+			return `
+	  --color-primary-800: ${color.secondary[8]};
+	  --color-primary-700: ${color.secondary[7]};
+	  --color-primary-600: ${color.secondary[6]};
+	  --color-primary-500: ${color.secondary[5]};
+	  --color-primary-400: ${color.secondary[4]};
+	  --color-primary-300: ${color.secondary[3]};
+	  --color-primary-200: ${color.secondary[2]};
+	  --color-primary-100: ${color.secondary[1]};
+	  --color-primary-050: ${color.secondary[0]};
+
+	  --color-secondary-050: ${color.primary[8]};
+	  --color-secondary-100: ${color.primary[7]};
+	  --color-secondary-200: ${color.primary[6]};
+	  --color-secondary-300: ${color.primary[5]};
+	  --color-secondary-400: ${color.primary[4]};
+	  --color-secondary-500: ${color.primary[3]};
+	  --color-secondary-600: ${color.primary[2]};
+	  --color-secondary-700: ${color.primary[1]};
+	  --color-secondary-800: ${color.primary[0]};
+
+	  --color-level-000: ${color.level[10]};
+	  --color-level-050: ${color.level[9]};
+	  --color-level-100: ${color.level[8]};
+	  --color-level-200: ${color.level[7]};
+	  --color-level-300: ${color.level[6]};
+	  --color-level-400: ${color.level[5]};
+	  --color-level-500: ${color.level[4]};
+	  --color-level-600: ${color.level[3]};
+	  --color-level-700: ${color.level[2]};
+	  --color-level-800: ${color.level[1]};
+	  --color-level-900: ${color.level[0]};
+	`
+		case "invert":
+			return `
+	  --color-primary-800: ${color.primary[8]};
+	  --color-primary-700: ${color.primary[7]};
+	  --color-primary-600: ${color.primary[6]};
+	  --color-primary-500: ${color.primary[5]};
+	  --color-primary-400: ${color.primary[4]};
+	  --color-primary-300: ${color.primary[3]};
+	  --color-primary-200: ${color.primary[2]};
+	  --color-primary-100: ${color.primary[1]};
+	  --color-primary-050: ${color.primary[0]};
+
+	  --color-secondary-050: ${color.secondary[8]};
+	  --color-secondary-100: ${color.secondary[7]};
+	  --color-secondary-200: ${color.secondary[6]};
+	  --color-secondary-300: ${color.secondary[5]};
+	  --color-secondary-400: ${color.secondary[4]};
+	  --color-secondary-500: ${color.secondary[3]};
+	  --color-secondary-600: ${color.secondary[2]};
+	  --color-secondary-700: ${color.secondary[1]};
+	  --color-secondary-800: ${color.secondary[0]};
+
+	  --color-level-000: ${color.level[10]};
+	  --color-level-050: ${color.level[9]};
+	  --color-level-100: ${color.level[8]};
+	  --color-level-200: ${color.level[7]};
+	  --color-level-300: ${color.level[6]};
+	  --color-level-400: ${color.level[5]};
+	  --color-level-500: ${color.level[4]};
+	  --color-level-600: ${color.level[3]};
+	  --color-level-700: ${color.level[2]};
+	  --color-level-800: ${color.level[1]};
+	  --color-level-900: ${color.level[0]};
+	`
+		case "none":
+		default:
+			return `
+	  --color-primary-800: ${color.primary[0]};
+	  --color-primary-700: ${color.primary[1]};
+	  --color-primary-600: ${color.primary[2]};
+	  --color-primary-500: ${color.primary[3]};
+	  --color-primary-400: ${color.primary[4]};
+	  --color-primary-300: ${color.primary[5]};
+	  --color-primary-200: ${color.primary[6]};
+	  --color-primary-100: ${color.primary[7]};
+	  --color-primary-050: ${color.primary[8]};
+
+	  --color-secondary-050: ${color.secondary[0]};
+	  --color-secondary-100: ${color.secondary[1]};
+	  --color-secondary-200: ${color.secondary[2]};
+	  --color-secondary-300: ${color.secondary[3]};
+	  --color-secondary-400: ${color.secondary[4]};
+	  --color-secondary-500: ${color.secondary[5]};
+	  --color-secondary-600: ${color.secondary[6]};
+	  --color-secondary-700: ${color.secondary[7]};
+	  --color-secondary-800: ${color.secondary[8]};
+
+	  --color-level-000: ${color.level[0]};
+	  --color-level-050: ${color.level[1]};
+	  --color-level-100: ${color.level[2]};
+	  --color-level-200: ${color.level[3]};
+	  --color-level-300: ${color.level[4]};
+	  --color-level-400: ${color.level[5]};
+	  --color-level-500: ${color.level[6]};
+	  --color-level-600: ${color.level[7]};
+	  --color-level-700: ${color.level[8]};
+	  --color-level-800: ${color.level[9]};
+	  --color-level-900: ${color.level[10]};
+	`
+	}
+}
+
+setThemeColor.propTypes = {
+	s: PropTypes.string.isRequired,
+}
+setThemeColor.defaultProps = {
+	s: PropTypes.oneOf(["switch", "switch-invert", "invert", "none"]),
+}
