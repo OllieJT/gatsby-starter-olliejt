36c336ea345e6a82e6a3a4ce9b9746090dc6aed2
Ollie Taylor <contact@olliejt.com>
2020-02-02 03:20:49 +0100

    Fixed @Types

--- src/@types/styled-components.d.ts
+++ src/@types/styled-components.d.ts
@@ -1,25 +1,8 @@
 import 'styled-components';
 
-type ThemeSize = {
-	smallest: string;
-	small: string;
-	medium: string;
-	large: string;
-	largeest: string;
-};
-type ThemeColor = {
-	lightest: string;
-	lighter: string;
-	light: string;
-	medium: string;
-	dark: string;
-	darker: string;
-	darkest: string;
-};
-
 declare module 'styled-components' {
 	export interface DefaultTheme {
-		mode: 'light' | 'dark';
+		mode: string;
 		setTheme: Function;
 	}
 }
